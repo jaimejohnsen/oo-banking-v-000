71c01e72ecda9df3adc408077daf9b94383e292c
Jaime Johnsen <drivetraindiva@gmail.com>
2020-08-11 17:06:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,35 +1,35 @@
 class Transfer
-
-attr_accessor :sender, :receiver, :status, :amount
-
-def initialize(sender, receiver, transfer)
-  @sender = sender
-  @receiver = receiver
-  @amount = transfer
-  @status = "pending"
-  @broke = 0
-  @closed = "closed"
-end
-
-def valid?
-  @sender.valid? && @receiver.valid?
-end
-
-def execute_transaction
-  if @status == "pending"
-    if @sender.balance >= @amount
-      @sender.deposit(-@amount)
-      @receiver.deposit(@amount)
-      @status = "complete"
-    else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, transfer)
+    @sender = sender
+    @receiver = receiver
+    @amount = transfer
+    @status = "pending"
+    @broke = 0
+    @closed = "closed"
+  end
+  
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+  
+  def execute_transaction
+    if @status == "pending"
+      if @sender.balance >= @amount
+        @sender.deposit(-@amount)
+        @receiver.deposit(@amount)
+        @status = "complete"
+      else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
     end
   end
-end
-
-def reverse_transfer
-
-end
-
+  
+  def reverse_transfer
+    
+  end
+  
 end
